03f1b4c7af953c74dfbd2d201d217f8a35513a47
Mohsan Ali <mohsanali1132@gmail.com>
2022-09-01 00:10:42 +0500

    removed console.logs(signin)

--- Server.js
+++ Server.js
@@ -73,14 +73,12 @@ app.post("/signin", (req, res) => {
     .where("email", "=", req.body.email)
     .then((data) => {
       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      console.log(isValid);
       if (isValid) {
         return db
           .select("*")
           .from("users")
           .where("email", "=", req.body.email)
           .then((user) => {
-            console.log(user);
             res.json(user[0]);
           })
           .catch((err) => res.status(400).json("wrong credentials."));
