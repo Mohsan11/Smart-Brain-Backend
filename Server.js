63b590993d7051e6f736272c6e4325f1e67196fb
Mohsan Ali <mohsanali1132@gmail.com>
2022-09-03 16:08:46 +0500

    Resolving Port

--- Server.js
+++ Server.js
@@ -49,7 +49,7 @@ app.put("/image", (req, res) => {
   image.imageHandle(req, res, db);
 });
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 300, () => {
   console.log(`This is working on Server ${process.env.PORT}`);
 });
 
